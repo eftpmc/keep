1cf293841ae2c124b63f9dbf1f797a83d68ced60
ari <61393419+eftpmc@users.noreply.github.com>
2023-11-26 17:08:23 -0500

    updated styling

--- components/Home.tsx
+++ components/Home.tsx
@@ -35,25 +35,33 @@ const Home: React.FC = () => {
 
   return (
     <div>
-      <input
-        type="text"
-        value={newTitle}
-        onChange={(e) => setNewTitle(e.target.value)}
-        placeholder="Enter title"
-      />
-      <input
-        key={inputKey}
-        type="file"
-        onChange={handleImageChange}
-      />
-      <AddButton onAdd={addCard} />
+<div className="flex flex-col items-center gap-4 mb-4 p-4 border border-gray-300 rounded-md bg-gray-800">
+        <input
+          className="form-input px-4 py-2 bg-gray-700 text-white rounded-md"
+          type="text"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          placeholder="Enter title"
+        />
+        <input
+          className="form-input px-4 py-2 bg-gray-700 text-white rounded-md file:bg-gray-600 file:border-none file:text-white"
+          key={inputKey}
+          type="file"
+          onChange={handleImageChange}
+        />
+            <button onClick={addCard}>
+      Add Item
+    </button>
+      </div>
 
       <div className="flex flex-wrap gap-4">
         {cards.map((card, index) => (
           <div key={index} className="flex flex-col items-center w-36">
-            <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 flex justify-center items-center">
-              {card.imageUrl ? <img src={card.imageUrl} alt={card.title} className="max-w-full max-h-full" /> : "No Image"}
-            </div>
+            {card.imageUrl && (
+              <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 flex justify-center items-center">
+                <img src={card.imageUrl} alt={card.title} className="max-w-full max-h-full" />
+              </div>
+            )}
             <div className="mt-2 text-center">{card.title}</div>
             <button
               onClick={() => removeCard(index)}
