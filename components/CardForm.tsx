2b991e4af433eb208c7cb5ce2ffdea2e848fe4e6
SaturnEFTPMC <zackhhi@gmail.com>
2023-12-02 11:41:03 -0500

    cardform sizing

--- components/CardForm.tsx
+++ components/CardForm.tsx
@@ -39,7 +39,7 @@ const CardForm: React.FC<CardFormProps> = ({ onSubmit, onTitleChange, onImageCha
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 mb-4 p-4 border border-gray-300 rounded-md bg-background w-full sm:w-auto">
+    <div className="flex flex-col bg-background items-center gap-4 mb-4 p-4 border rounded-md mx-auto w-full sm:max-w-sm md:max-w-md lg:max-w-lg animate-in">
       <input
         className="form-input px-4 py-2 bg-gray-700 text-white rounded-md"
         type="text"
