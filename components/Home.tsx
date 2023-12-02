4ad3bc9e00436e427171605337344a8417452ebe
ari <61393419+eftpmc@users.noreply.github.com>
2023-12-01 23:03:01 -0500

    cloud image upload

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,6 +3,9 @@
 import React, { useState, useEffect } from 'react';
 import Card from '@/components/Card';
 import CardForm from '@/components/CardForm';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_SERVICE!);
 
 type CardType = {
   id?: number;
@@ -17,6 +20,24 @@ const Home: React.FC<{ isModalOpen: boolean; hideModal: () => void }> = ({ isMod
   const [newImage, setNewImage] = useState('');
   const [inputKey, setInputKey] = useState(Date.now());
 
+  const fetchImage = async (imagePath: string) => {
+    try {
+      const { data, error } = await supabase.storage
+        .from("card-images")
+        .download(imagePath);
+      if (error) {
+        throw error;
+      }
+      
+      const imageUrl = URL.createObjectURL(data);
+  
+      return imageUrl;
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      return null;
+    }
+  };  
+
   useEffect(() => {
     const fetchCards = async () => {
       try {
@@ -25,7 +46,15 @@ const Home: React.FC<{ isModalOpen: boolean; hideModal: () => void }> = ({ isMod
           throw new Error('Network response was not ok');
         }
         const fetchedCards = await response.json();
-        setCards(fetchedCards);
+        
+        const cardsWithNewImages = await Promise.all(
+          fetchedCards.map(async (card: { imageUrl: string; }) => {
+            const newImageUrl = await fetchImage(card.imageUrl);
+            return { ...card, imageUrl: newImageUrl };
+          })
+        );
+  
+        setCards(cardsWithNewImages);
       } catch (error) {
         console.error('Error fetching cards:', error);
       }
@@ -34,9 +63,25 @@ const Home: React.FC<{ isModalOpen: boolean; hideModal: () => void }> = ({ isMod
     fetchCards();
   }, []);
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
-      setNewImage(URL.createObjectURL(event.target.files[0]));
+      const file = event.target.files[0];
+
+      const { data, error } = await supabase.storage
+      .from('card-images')
+      .upload(`images/${file.name}`, file, {
+        cacheControl: '3600',
+        upsert: false
+      });
+
+    if (error) {
+      console.error('Error uploading image:', error);
+      return;
+    }
+
+      //const imageUrl = URL.createObjectURL(event.target.files[0])
+      const imageUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/card-images/${data.path}`;
+      setNewImage(data.path);
     }
   };
 
@@ -58,6 +103,11 @@ const Home: React.FC<{ isModalOpen: boolean; hideModal: () => void }> = ({ isMod
 
         const responseData = await response.json();
         const addedCard =  responseData[0];
+
+        const newImageUrl = await fetchImage(addedCard.imageUrl);
+        if (newImageUrl) {
+          addedCard.imageUrl = newImageUrl;
+        }
 
         setCards([...cards, addedCard]);
         setNewTitle('');
