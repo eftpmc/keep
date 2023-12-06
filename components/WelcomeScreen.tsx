eac20af3c70a0f2ec3a86ee9f68106b0d4e5d267
ari <61393419+eftpmc@users.noreply.github.com>
2023-12-05 21:44:58 -0500

    responsive landing page

--- components/WelcomeScreen.tsx
+++ components/WelcomeScreen.tsx
@@ -2,11 +2,10 @@ import Link from 'next/link'
 
 export default function WelcomeScreen() {
   return (
-    <div className="flex flex-col gap-6">
-      <p className="text-center">
+    <div className="flex flex-col gap-6 p-4">
+      <p className="text-center text-lg">
         Discover the easiest way to manage your thoughts, keep track of tasks, and understand yourself better.
       </p>
-
       <div className="text-center mt-6">
         Already have an account? 
         <Link href="/login">
