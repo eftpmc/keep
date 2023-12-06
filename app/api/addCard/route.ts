40b419bc2aad3ec634f9e16f43d0a9736bf3debb
ari <61393419+eftpmc@users.noreply.github.com>
2023-12-05 22:10:58 -0500

    cards are now user specific 😭😭

--- app/api/addCard/route.ts
+++ app/api/addCard/route.ts
@@ -3,26 +3,46 @@ import { createClient } from '@/utils/supabase/server';
 
 export async function POST(req: Request) {
     const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
 
     try {
-        const supabase = createClient(cookieStore);
-        const { title, imageUrl, description, link, createdDate } = await req.json();
+        // Retrieve user from the current session
+        const { data: { user } } = await supabase.auth.getUser()
 
+        if (!user) {
+            throw new Error('No authenticated user found.');
+        }
+
+        const userId = user.id;
+
+        // Parse the request body
+        const { title, imageUrl, description, link, createdDate } = await req.json();
         const createdDateObj = new Date(createdDate);
 
+        // Insert the card with the user ID
         const { data, error } = await supabase
             .from('cards')
             .insert([
-                { title, link, imageUrl, description, createdDate: createdDateObj.toISOString() },
+                {
+                    title,
+                    link,
+                    imageUrl,
+                    description,
+                    createdDate: createdDateObj.toISOString(),
+                    user_id: userId,
+                },
             ])
-            .select()
+            .select();
 
-        if (error) throw error;
-        return new Response(JSON.stringify(data), { status: 200, headers: { "Content-Type": "application/json" } })
+        if (error) {
+            throw error;
+        }
+
+        return new Response(JSON.stringify(data), { status: 200, headers: { "Content-Type": "application/json" } });
     } catch (error) {
         if (error instanceof Error) {
-            return new Response(JSON.stringify({ message: error.message }), { status: 500, headers: { "Content-Type": "application/json" } })
+            return new Response(JSON.stringify({ message: error.message }), { status: 500, headers: { "Content-Type": "application/json" } });
         }
-        return new Response(JSON.stringify({ message: "An unknown error occurred" }), { status: 500, headers: { "Content-Type": "application/json" } })
+        return new Response(JSON.stringify({ message: "An unknown error occurred" }), { status: 500, headers: { "Content-Type": "application/json" } });
     }
 };
--- app/api/getCards/route.ts
+++ app/api/getCards/route.ts
@@ -3,12 +3,21 @@ import { createClient } from '@/utils/supabase/server';
 
 export async function GET(req: Request) {
     const cookieStore = cookies();
-    const { searchParams } = new URL(req.url)
-    let date = searchParams.get('date')
+    const { searchParams } = new URL(req.url);
+    let date = searchParams.get('date');
     let timezoneOffset = parseInt(searchParams.get('timezoneOffset') || '0', 10);
 
     try {
         const supabase = createClient(cookieStore);
+
+        // Retrieve user from the current session
+        const { data: { user } } = await supabase.auth.getUser()
+
+        if (!user) {
+            throw new Error('No authenticated user found.');
+        }
+
+        const userId = user.id; // This is the user's UUID
 
         const startDate = new Date(`${date}T00:00:00Z`);
         startDate.setUTCMinutes(startDate.getUTCMinutes() + timezoneOffset);
@@ -19,14 +28,17 @@ export async function GET(req: Request) {
         let { data: cards, error } = await supabase
             .from('cards')
             .select('*')
+            .eq('user_id', userId)
             .gte('createdDate', startDate.toISOString())
             .lt('createdDate', endDate.toISOString());
 
-        return new Response(JSON.stringify(cards), { status: 200, headers: { "Content-Type": "application/json" } })
+        if (error) throw error;
+
+        return new Response(JSON.stringify(cards), { status: 200, headers: { "Content-Type": "application/json" } });
     } catch (error) {
         if (error instanceof Error) {
-            return new Response(JSON.stringify({ message: error.message }), { status: 500, headers: { "Content-Type": "application/json" } })
+            return new Response(JSON.stringify({ message: error.message }), { status: 500, headers: { "Content-Type": "application/json" } });
         }
-        return new Response(JSON.stringify({ message: "An unknown error occurred" }), { status: 500, headers: { "Content-Type": "application/json" } })
+        return new Response(JSON.stringify({ message: "An unknown error occurred" }), { status: 500, headers: { "Content-Type": "application/json" } });
     }
 };
