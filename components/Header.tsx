cd3b48a3ec847bc4354de010b68440cda92dabb8
ari <61393419+eftpmc@users.noreply.github.com>
2023-12-06 19:48:10 -0500

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,20 +5,23 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        <a href="https://supabase.com" target="_blank" rel="noreferrer">
+        <a
+          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+          target="_blank"
+          rel="noreferrer"
+        >
           <SupabaseLogo />
         </a>
         <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
           <NextLogo />
         </a>
       </div>
-      {/* <MascotImage className="h-32 w-32" />  Mascot Image */}
       <h1 className="text-4xl lg:text-5xl font-bold text-center">
         Keep
       </h1>
       <p className="text-xl lg:text-2xl text-center max-w-2xl mx-auto mt-4">
-        Keep yourself together with our daily card organizer.
+        Keep yourself together.
       </p>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
     </div>
