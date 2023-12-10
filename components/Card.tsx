736cfc368176305503a3ba3b844437dae5d79e90
ari <61393419+eftpmc@users.noreply.github.com>
2023-12-10 17:22:28 -0500

    sheet on bottom side

--- components/Card.tsx
+++ components/Card.tsx
@@ -99,7 +99,7 @@ const Card: React.FC<CardProps> = ({ card, onRemove, onEdit }) => {
           </div>
         </div>
       </div>
-      <SheetContent>
+      <SheetContent side="bottom">
         <SheetHeader>
           <SheetTitle>Edit Card</SheetTitle>
         </SheetHeader>
