ad92eb6308789f056489a5d6e36bccfb54baa971
ari <61393419+eftpmc@users.noreply.github.com>
2023-12-11 20:13:02 -0500

    nav removed

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,6 @@ import WelcomeScreen from '@/components/WelcomeScreen';
 import Home from '@/components/Home';
 import Header from '@/components/Header';
 import DatePicker from '@/components/DatePicker';
-import { Button } from "@/components/ui/button"
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-import { LuAlignJustify } from "react-icons/lu";
 
 export default function Index() {
   const { isAuth } = useAuth();
@@ -25,62 +15,43 @@ export default function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-5 items-center">
-      <Sheet>
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <div className="flex-1 flex justify-start items-center gap-4">
-              <SheetTrigger asChild>
-                <Button className="sm:hidden">
-                  <LuAlignJustify size={20} />
-                </Button>
-              </SheetTrigger>
-              <div className="hidden sm:flex gap-4">
-                <UniversalButton text="Home" href="/" ariaLabel="Navigate to Home" />
-                <UniversalButton text="Tags" href="/tags" ariaLabel="Navigate to Tags" />
-              </div>
-            </div>
-            <div className="flex-1 flex justify-center items-center">
-              <DatePicker
-                selectedDate={selectedDate}
-                setSelectedDate={setSelectedDate}
-              />
-            </div>
-            <div className="flex-1 flex justify-end items-center">
-              <AuthButton />
-            </div>
-          </div>
-        </nav>
-
-        <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl">
-          {!isAuth ? <Header /> : null}
-          <main className="flex-1 flex flex-col gap-6">
-            {isAuth ? <Home selectedDate={selectedDate} /> : <WelcomeScreen />}
-          </main>
-        </div>
-
-        <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-          <p>
-            Built by{' '}
-            <a href="https://twitter.com/eftpmc" target="_blank" className="font-bold hover:underline" rel="noreferrer">
-              ari
-            </a>
-            . The source code is available on{' '}
-            <a href="https://github.com/eftpmc/keep" target="_blank" className="font-bold hover:underline" rel="noreferrer">
-              GitHub
-            </a>
-            .
-          </p>
-        </footer>
-        <SheetContent side="top">
-          <SheetHeader>
-            <SheetTitle>Nav Menu</SheetTitle>
-          </SheetHeader>
-          <div className="p-4 space-y-4">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <div className="flex-1 flex justify-start items-center gap-4">
             <UniversalButton text="Home" href="/" ariaLabel="Navigate to Home" />
-            <UniversalButton text="Tags" href="/tags" ariaLabel="Navigate to Tags" />
           </div>
-        </SheetContent>
-      </Sheet>
+          <div className="flex-1 flex justify-center items-center">
+            <DatePicker
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+            />
+          </div>
+          <div className="flex-1 flex justify-end items-center">
+            <AuthButton />
+          </div>
+        </div>
+      </nav>
+
+      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl">
+        {!isAuth ? <Header /> : null}
+        <main className="flex-1 flex flex-col gap-6">
+          {isAuth ? <Home selectedDate={selectedDate} /> : <WelcomeScreen />}
+        </main>
+      </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Built by{' '}
+          <a href="https://twitter.com/eftpmc" target="_blank" className="font-bold hover:underline" rel="noreferrer">
+            ari
+          </a>
+          . The source code is available on{' '}
+          <a href="https://github.com/eftpmc/keep" target="_blank" className="font-bold hover:underline" rel="noreferrer">
+            GitHub
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 }
